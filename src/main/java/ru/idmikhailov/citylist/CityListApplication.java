91f3ae14597f5556572e391361adbf4b3d8015e2
Ilya Mikhailov <fun4lsd@gmail.com>
2022-10-06 23:13:15 +0400

    - swagger added
    TODO:
    - write some tests

--- src/main/java/ru/idmikhailov/citylist/CityListApplication.java
+++ src/main/java/ru/idmikhailov/citylist/CityListApplication.java
@@ -2,6 +2,7 @@ package ru.idmikhailov.citylist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
 @SpringBootApplication
 public class CityListApplication {
