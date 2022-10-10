dab5f6c023bafececbe003721a62ca3d3ec3f946
Ilya Mikhailov <fun4lsd@gmail.com>
2022-10-10 17:25:00 +0400

    - get user added

--- src/main/java/ru/idmikhailov/citylist/controller/AuthController.java
+++ src/main/java/ru/idmikhailov/citylist/controller/AuthController.java
@@ -4,8 +4,11 @@ package ru.idmikhailov.citylist.controller;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.idmikhailov.citylist.dto.UserDto;
@@ -27,5 +30,15 @@ public class AuthController {
         String token = tokenService.generateToken(authentication);
         log.debug("Token granted {}", token);
         return new UserWithTokenDto(token, new UserDto(authentication.getName(), ((User)authentication.getPrincipal()).getPassword()));
+    }
+
+
+    @GetMapping("/me")
+    public UserWithTokenDto getMe(@AuthenticationPrincipal Jwt principal) {
+        if (principal == null) return null;
+        log.debug("User requested for token: '{}'", principal.getTokenValue());
+        UserWithTokenDto user = new UserWithTokenDto(principal.getTokenValue(), new UserDto(principal.getClaimAsString("sub"), null));
+        log.debug("User granted {}", user);
+        return user;
     }
 }
